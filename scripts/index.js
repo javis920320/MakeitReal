d7e515925a039036bfd169166b047d410fa9dee6
javis920320 <javis9203@gmail.com>
2019-03-05 20:21:32 -0500

    terminamos
    terminamos

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,52 @@
  */
 
 console.log("JavaScript is amazing!");
+
+dato=Count("HolaH mundo HipermegaH","H");
+
+console.log(dato);
+
+function Count(str,char){
+
+ 	return   str.split(char).length-1;
+
+ }
+
+
+
+arreglo = new Array(1,20,60,23,16,2,6,9);
+
+
+ miarreglo=Filter(arreglo);
+ console.log(miarreglo);
+
+
+
+  function Filter(arreglo){
+  	res= new
+  	Array();
+
+
+	longitud=arreglo.length;
+
+
+	for (var i =0; i<=longitud;i++ ) {
+	
+		if(arreglo[i]>=10){
+		res.push(arreglo[i]);
+
+		}
+									}
+  	return res;
+
+  
+  }
+
+
+ 
+   var  hipo=hipotenusa(3,4);
+    console.log('Hipotesusa='+hipo);
+
+ function hipotenusa(a,b){
+ 	return Math.hypot(a,b)
+ }
